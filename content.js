47cb3dad8f2bb36bea7bcddd1e95189d5706bf5a
Kevin Simper <kevin.simper@gmail.com>
2015-05-07 22:05:14 +0200

    Refactor and improve detection

--- content.js
+++ content.js
@@ -1,17 +1,32 @@
-$('body *').each(function(){
-  var text = $(this).text().trim();
+(function(){
   var combinations = ['[#', '[Finishes #']
+  var $selectorAll = $('body *')
+
+  var transform = function(){
+    var text = $(this).text().trim();
 
 
-  for(var i = 0; i < combinations.length; i++ ) {
-    var key = combinations[i];
+    for(var i = 0; i < combinations.length; i++ ) {
+      var key = combinations[i];
 
-    if(text.indexOf(key) !== -1 && $(this).children().length === 0){
-      var storyId = text.split(key)[1].split(']')[0]
-      var storyIdWithBrackets = key + storyId + ']';
-      var link = '<a href="https://www.pivotaltracker.com/story/show/' + storyId + '">' + storyIdWithBrackets + '</a>'
-      var textWithoutId = text.replace(key + storyId + ']', '');
-      $(this).html(link + textWithoutId);
+      if(text.indexOf(key) !== -1 && $(this).children().length === 0 && $(this).attr('href').indexOf('pivotaltracker') === -1){
+        var storyId = text.split(key)[1].split(']')[0]
+        var storyIdWithBrackets = key + storyId + ']';
+
+        var url = 'https://www.pivotaltracker.com/story/show/' + storyId
+        var link = $('<a></a>')
+          .attr('href', url)
+          .text(storyIdWithBrackets)
+          .click(function(){
+            window.location = url
+          })
+
+        var textWithoutId = text.replace(key + storyId + ']', '');
+        $(this).empty().append(link).append($('<span></span>').text(textWithoutId));
+      }
     }
-  }
-})
+  };
+
+  $selectorAll.each(transform)
+
+})();
